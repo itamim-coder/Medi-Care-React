10e4a57bbce9f4f19d89b94f299a084c5a7ccf71
tamsam12202 <itamim12202@gmail.com>
2021-10-18 21:59:04 +0600

    json update

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -5,7 +5,7 @@ import './Services.css'
 const Services = () => {
     const [services, setServices] = useState([])
     useEffect(()=>{
-        fetch('service.json')
+        fetch('db.json')
         .then(res => res.json())
         .then(data => setServices(data));
     },[])
